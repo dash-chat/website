3436c9605a0f81d82a6b9b5702c99b7aeb3ce48e
jade <jade@hopepunk.me>
2026-06-18 14:22:42 +1000

    add nojekyll file to the dist output

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import fs from 'node:fs'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -8,13 +10,23 @@ export default defineConfig({
       }
     }
   },
-  plugins: [{
-    name: 'blog-routing',
-    configureServer(server) {
-      server.middlewares.use((req, res, next) => {
-        if (req.url === '/blog') req.url = '/blog.html';
-        next();
-      });
+  plugins: [
+    {
+      name: 'blog-routing',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/blog') req.url = '/blog.html';
+          next();
+        });
+      }
+    },
+    {
+      name: 'ensure-nojekyll',
+      apply: 'build',
+      closeBundle() {
+        const outPath = path.resolve(process.cwd(), 'dist', '.nojekyll');
+        fs.writeFileSync(outPath, '');
+      }
     }
-  }]
+  ]
 })
